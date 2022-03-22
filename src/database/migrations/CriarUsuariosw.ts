e2c812baae6c60c70068707e539d22fee02ab616
wesley lopes <nobretecnologia@gmail.com>
2022-03-21 23:44:12 -0300

    Aula 9

---
+++ src/database/migrations/CriarUsuariosw.ts
@@ -0,0 +1,37 @@
+import { IsNull, MigrationInterface, QueryRunner, Table } from "typeorm"
+
+export class CriarUsuarios1647914094602 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.createTable(
+          new Table(
+              {
+                  name: 'usuarios',
+                  columns: [{
+                      name:'id',
+                      type:'string',
+                      isPrimary:true,
+                      isNullable:false
+                  },
+                  {
+                     name:'nome',
+                     type:'string',
+                     isNullable:false
+                  },
+                  {
+                     name:'email',
+                     type:'string',
+                     isNullable:true
+                  }    
+                ]
+
+              }
+          )
+      )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        queryRunner.dropTable('usuarios');
+    }
+
+}
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,20 @@
-import express, {Request, Response} from 'express';
-import { router } from './routes';
+import { AppDataSource } from "./data-source"
+import { User } from "./entity/User"
 
-const server = express();
+AppDataSource.initialize().then(async () => {
 
-server.use(express.json());
-server.use(router);
+    console.log("Inserting a new user into the database...")
+    const user = new User()
+    user.firstName = "Timber"
+    user.lastName = "Saw"
+    user.age = 25
+    await AppDataSource.manager.save(user)
+    console.log("Saved a new user with id: " + user.id)
 
+    console.log("Loading users from the database...")
+    const users = await AppDataSource.manager.find(User)
+    console.log("Loaded users: ", users)
 
-server.listen(5000),()=>{
-  console.log('Servidor on na porta 5000'); 
+    console.log("Here you can setup and run express / fastify / any other framework.")
 
-};
-
-//Criar Usuario 
-//Selecionar Usuario 
-//Editar Usuarios 
-//Deletar Usuario 
-
-//Get
-//Put/Patch -> Atualizar
-//Update -> Update
-//Delete
+}).catch(error => console.log(error))
